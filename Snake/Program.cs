4cb160716f6c7058e51de7dd170bf18c4d569c5f
Yaroslav Yekasov <fngpr.56@gmail.com>
2023-05-14 17:38:37 +0300

    11. code upgrade

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -24,28 +24,17 @@ namespace Snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-            snake.Move();
 
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-
-
-
-            Console.ReadLine();
+            while (true)
+            {
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandlKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
         }
     }
 }
--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Snake
 {
     class Snake : Figure
     {
         Direction direction;
-        public Snake(Point tail, int lenght, Direction direction)
+        public Snake(Point tail, int lenght, Direction _direction)
         {
+            direction = _direction;
             pList = new List<Point>();
             for (int i = 0; i < lenght; i++)
             {
@@ -36,6 +38,18 @@ namespace Snake
             Point nextPoint = new Point(head);
             nextPoint.Move(1, direction);
             return nextPoint;
-        }            
+        }      
+        
+        public void HandlKey(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow)
+                direction = Direction.LEFT;
+            else if (key == ConsoleKey.RightArrow)
+                direction = Direction.RIGHT;
+            else if (key == ConsoleKey.DownArrow)
+                direction = Direction.DOWN;
+            else if (key == ConsoleKey.UpArrow)
+                direction = Direction.UP;
+        }
     }
 }

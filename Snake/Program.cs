dd18703c72496ac0c5a8e1829602ef737a00f19a
Yaroslav Yekasov <fngpr.56@gmail.com>
2023-05-14 22:03:26 +0300

    14. snake hit

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,30 +10,32 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            VerticalLine v1 = new VerticalLine(0,10,5,'%');
-            Draw(v1);
+            //VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
+            //Draw(v1);
 
-            Point p = new Point(4, 5, '*');
-            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
-            Draw(fSnake);
-            Snake snake = (Snake) fSnake;
+            //Point p = new Point(4, 5, '*');
+            //Figure fSnake = new Snake(p, 4, Direction.RIGHT);
+            //Draw(fSnake);
+            //Snake snake = (Snake)fSnake;
 
-            HorizontalLine h1 = new HorizontalLine(0,5,6,'&');
+            //HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
 
-            List<Figure> figures = new List<Figure>();
-            figures.Add(fSnake);
-            figures.Add(v1);
-            figures.Add(h1);
+            //List<Figure> figures = new List<Figure>();
+            //figures.Add(fSnake);
+            //figures.Add(v1);
+            //figures.Add(h1);
 
-            foreach(var f in figures)
-            {
-                f.Draw();
-            }
+            //foreach (var f in figures)
+            //{
+            //    f.Draw();
+            //}
 
-            static void Draw(Figure figure)
-            {
-                figure.Draw();
-            }
+
+            //static void Draw(Figure figure)
+            //{
+            //    figure.Draw();
+            //}
+
 
             //HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
             //HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
@@ -44,38 +46,44 @@ namespace Snake
             //leftLine.Draw();
             //rightLine.Draw();
 
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
-            //Point p = new Point(4, 5, '*');
-            //Snake snake = new Snake(p, 4, Direction.RIGHT);
-            //snake.Draw();
+            FoodCreator foodCreator = new FoodCreator(80, 25, '%');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
 
-            //FoodCreator foodCreator = new FoodCreator(80, 25, '%');
-            //Point food = foodCreator.CreateFood();
-            //food.Draw();
+            while (true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
 
-            //while (true)
-            //{
-            //    if(snake.Eat(food))
-            //    {
-            //        food = foodCreator.CreateFood();
-            //        food.Draw();
-            //    }
-            //    else
-            //    {
-            //        snake.Move();
-            //    
-            //    Thread.Sleep(100);
-            
-            //    if(Console.KeyAvailable)
-            //    {
-            //        ConsoleKeyInfo key = Console.ReadKey();
-            //        snake.HandleKey(key.Key);
-            //    }
-            //    Thread.Sleep(100);
-            //    snake.Move();
-            //}
-            Console.ReadLine();
+                Thread.Sleep(100);
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+                
+            }
         }
     }
 }
--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -39,6 +39,17 @@ namespace Snake
             nextPoint.Move(1, direction);
             return nextPoint;
         }      
+
+        internal bool IsHitTail()
+        {
+            var head = pList.Last();
+            for(int i = 0; i<pList.Count - 2; i++)
+            {
+                if (head.IsHit(pList[i]))
+                    return true;
+            }
+            return false;
+        }
         
         public void HandleKey(ConsoleKey key)
         {

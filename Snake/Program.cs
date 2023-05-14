40856930b032db73f4ac4e224e4fc10273af1f79
Yaroslav Yekasov <fngpr.56@gmail.com>
2023-05-14 18:37:40 +0300

    12. food

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -25,8 +25,24 @@ namespace Snake
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
 
+            FoodCreator foodCreator = new FoodCreator(80, 25, '%');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
             while (true)
             {
+                if(snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+            
                 if(Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();

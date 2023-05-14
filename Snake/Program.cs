59f2ca02255b9315485588c588c6386790ae59f3
Yaroslav Yekasov <fngpr.56@gmail.com>
2023-05-14 14:47:19 +0300

    09. abstraction

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -20,7 +20,8 @@ namespace Snake
             rightLine.Draw();
 
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
             Console.ReadLine();
         }
